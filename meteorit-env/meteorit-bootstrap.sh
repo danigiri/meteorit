09f595253b0fba12f79a69f8b2dd7dac90260fa3
Daniel Giribet <dani@calidos.cat>
2013-03-17 15:21:54 +0100

    backup...

--- meteorit-env/meteorit-bootstrap.sh
+++ meteorit-env/meteorit-bootstrap.sh
@@ -3,6 +3,8 @@
 echo 'Installing base stuff...'
 # http://wiki.centos.org/AdditionalResources/Repositories/RPMForge
 yum install -q -y wget
+
+# 
 wget -q 'http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm' -O rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 rpm -K rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
@@ -12,4 +14,22 @@ if [ $? != 0 ]; then
 fi
 rpm -i rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 
-yum install -q -y gcc
+# epel repo
+wget -q 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+rpm --import https://fedoraproject.org/static/0608B895.txt
+rpm -K epel-release-6-8.noarch.rpm
+if [ $? != 0 ]; then
+	echo 'Could not verify epel package'
+	exit 1
+fi
+rpm -Uvh epel-release-6*.rpm
+
+yum install -q -y gcc make pcre-devel zlib-devel
+yum install -q -y rpm-build man
+
+# enable port 80
+# rulepos=`/sbin/service iptables status | grep -P '\d+.*ACCEPT' | tail -1 | awk '{print $1}'`
+# rulepos=$(( $rulepos + 1 ))
+# iptables -I INPUT $rulepos -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+# /sbin/service iptables save
+# /sbin/service iptables restart
--- meteorit-frontend/src/main/bash/meteorit-frontend-compile.sh
+++ meteorit-frontend/src/main/bash/meteorit-frontend-compile.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+echo 'Starting lua build...'
+
+cd '${project.build.directory}/${lua.name_}'
+echo 'Compiling:'
+make PREFIX=${lua.installfolder_} | pv -l -f -p -s 6 > ./make.output
+echo 'Installing:'
+make install PREFIX=${lua.tempfolder_} | pv -l -f -p -s 18 >> ./make.output
+cd ${lua.tempfolder_}
+find . -type d -exec mkdir -vp ${lua.tempfolder2_}/'{}' \; 
+
+echo 'Starting nginx build...'
+
+cd '${nginx.sourcefolder_}'
+if [ -e objs/nginx ]; then
+	echo 'Already compiled, skipping'
+	exit 0
+fi
+
+echo 'Configuring:'
+./configure --prefix='${nginx.installfolder_}' \
+			--add-module='${redis2-module.sourcefolder_}/' \
+			--user='${nginx.username_}'\
+			--group='${nginx.groupname_}' | pv -f -l -p -s 115 > ./configure.output
+ERR_=$?
+if [ $ERR_ -ne 0 ]; then
+	echo "Error configuring, checkout 'configure.output'"
+	exit $ERR_
+fi
+echo 'Compiling:'
+make | pv -l -f -p -s 461 > ./make.output
+ERR_=$?
+if [ $ERR_ -ne 0 ]; then
+	echo "Error running make, checkout 'make.output'"
+	exit $ERR_
+fi
+
+# the make install script uses a prefix DESTDIR environment variable which we use to create a complete temp install
+DESTDIR='${nginx.tempfolder_}' make install
+
+echo 'Modifying configuration...'
+sed -i  -e 's/^#user.*nobody;/user ${nginx.username_};/' ${nginx.tempfolder_}/${nginx.installfolder_}/conf/nginx.conf
+
+echo 'Completed'
