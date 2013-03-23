824fc0d7de85cc804ba0944cf3889f8e1fd0cfe4
Daniel Giribet <dani@calidos.cat>
2013-03-23 14:05:35 +0100

    restructuring project frontend...

---
+++ meteorit-frontend-nginx/src/main/resources/preinstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# PREINSTALLATION script for meteorit-frontend
+
+#  Copyright 2013 Daniel Giribet <dani - calidos.cat>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+ # first time installation
+if [ "$1" -eq '1' ]; then
+
+	# add nginx nonprivileged user
+	echo 'Adding nginx daemon user'
+	id '${nginx.username_}' 2>/dev/null
+	retval=$?
+	[ $retval -eq 1 ] && /usr/sbin/useradd -c 'Nginx daemon' -M -r -s /sbin/nologin ${nginx.username_}
+
+fi
