f0d11d29828eb12a303f3cf833eb0a00f38b059b
Daniel Giribet <dani@calidos.cat>
2013-04-01 19:18:34 +0200

    nginx starts with lua OK

--- meteorit-frontend-lua/src/main/bash/meteorit-frontend-lua-compile.sh
+++ meteorit-frontend-lua/src/main/bash/meteorit-frontend-lua-compile.sh
@@ -3,17 +3,29 @@
 cd '${project.build.directory}/${lua.name_}'
 
 
-if [ -e src/luajit ]; then
-	echo 'Lua already built, skipping'
-	exit 0
-fi
+#if [ -e src/luajit ]; then
+#	echo 'Lua already built, skipping'
+#	exit 0
+#fi
 
 echo 'Starting lua build...'
 
 
 echo 'Compiling:'
-make amalg PREFIX=${lua.installfolder_} | pv -l -f -p -s 7 > ./make.output
+make amalg PREFIX=${lua.installfolder_} | pv -l -f -p -s 26  > ./make.output
 
 # the make install script uses a prefix DESTDIR environment variable which we use to create a complete temp install
 echo 'Installing:'
 make install PREFIX=${lua.installfolder_} DESTDIR=${lua.tempfolder_} | pv -l -f -p -s 18 >> ./make.output
+
+echo 'Starting LuaRedisParser build...'
+cd '${lua-redisparser.sourcefolder_}'
+make PREFIX=${lua.installfolder_} \
+	 LUA_INCLUDE_DIR="${lua.tempfolder_}/${lua.installfolder_}/include/luajit-2.0" \
+	 LUA_LIB_DIR=${lua.tempfolder_}/${lua.installfolder_}/lib/lua/5.1 | pv -l -f -p -s 1 >> ./make.output
+echo 'Installing:'
+make install DESTDIR=${lua.tempfolder_} LUA_LIB_DIR=${lua.installfolder_}/lib/lua/5.1 | pv -l -f -p -s 1 >> ./make.output
+# test fails as it needs a script called 'prove' which I can't seem to find 
+make test
+
+echo 'Completed'
--- meteorit-frontend-nginx/src/main/bash/meteorit-frontend-nginx-compile.sh
+++ meteorit-frontend-nginx/src/main/bash/meteorit-frontend-nginx-compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd '${nginx.sourcefolder_}'
-#if [ -e objs/nginx ]; then
-#	echo 'Nginx already built, skipping'
-#	exit 0
-#fi
+if [ -e objs/nginx ]; then
+	echo 'Nginx already built, skipping'
+	exit 0
+fi
 
 echo 'Starting nginx build...'
 
@@ -12,26 +12,30 @@ echo 'Starting nginx build...'
 echo 'Configuring:'
 
 # add LuaJIT environment
-export LUAJIT_LIB=${install.prefix_}/lj2/lib
-export LUAJIT_INC=${install.prefix_}/lj2/include/luajit-2.0
+export LUAJIT_LIB='${install.prefix_}/lj2/lib'
+export LUAJIT_INC='${install.prefix_}/lj2/include/luajit-2.0'
 
-if [[ ! -d "$LUAJIT_LIB" || ! -d "" ]]; then
+#if [[ ! -d "$LUAJIT_LIB" || ! -d "" ]]; then
+#
+#fi
 
-fi
-
+# we configure, taking care to add add the lua shared library to the binary lookup path
 ./configure --prefix='${nginx.installfolder_}' \
+			--with-ld-opt="-Wl,-rpath,$LUAJIT_LIB" \
 			--add-module='${redis2-module.sourcefolder_}/' \
 			--add-module='${nginx-devel-kit.sourcefolder_}' \
 			--add-module='${nginx-lua-module.sourcefolder_}' \
 			--user='${nginx.username_}'\
-			--group='${nginx.groupname_}' | pv -f -l -p -s 122 > ./configure.output
+			--group='${nginx.groupname_}' 
+			#| pv -f -l -p -s 122 > ./configure.output
 ERR_=$?
 if [ $ERR_ -ne 0 ]; then
 	echo "Error configuring, checkout 'configure.output'"
 	exit $ERR_
 fi
 echo 'Compiling:'
-make | pv -l -f -p -s 641 > ./make.output
+make
+#| pv -l -f -p -s 641 > ./make.output
 ERR_=$?
 if [ $ERR_ -ne 0 ]; then
 	echo "Error running make, checkout 'make.output'"
@@ -42,6 +46,7 @@ fi
 DESTDIR='${nginx.tempfolder_}' make install
 
 echo 'Modifying configuration...'
-sed -i  -e 's/^#user.*nobody;/user ${nginx.username_};/' ${nginx.tempfolder_}/${nginx.installfolder_}/conf/nginx.conf
+sed -i -e 's/^#user.*nobody;/user ${nginx.username_};/' ${nginx.tempfolder_}/${nginx.installfolder_}/conf/nginx.conf
 
 echo 'Completed'
+
