404c6f93af126e8947ff0f278584bdd1a618ee40
Daniel Giribet <dani@calidos.cat>
2013-11-24 23:47:17 +0100

    Cool, first version of storm.yaml utils :)

--- meteorit-backend-storm/src/main/bash/meteorit-backend-storm-setup.sh
+++ meteorit-backend-storm/src/main/bash/meteorit-backend-storm-setup.sh
@@ -16,8 +16,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-. ${install.prefix_}/share/meteorit/meteorit-common
-
+#. ${install.prefix_}/share/meteorit/meteorit-common
+. /opt/share/meteorit/meteorit-common
 
 ################################################################################
 print_usage_exit() {
@@ -31,9 +31,9 @@ print_usage_exit() {
 
 
 ################################################################################
-error_and_exit() {
+error_and_exit_() {
 	#<message> <code>
-	printf "$1\n" >&2
+	printf "$1\n" 1>&2
  	exit $2
 }
 
@@ -42,50 +42,90 @@ error_and_exit() {
 process_yaml_file_parameter_() {
 	# <file> 
 	
- 	if [ $# -eq 0 ]; then
- 		error_and_exit 'No yaml config file passed' 1
+ 	if [ "$#" -eq '0' ]; then
+ 		error_and_exit_ 'No yaml config file passed' 1
  	fi
  
  	f_="$1"
  	shift
  	
  	if [ ! -e "$f_" ]; then
- 		printf "Can't find yaml config file ($f_)\n" >&2
- 		exit 1
+ 		error_and_exit_ "Can't find yaml config file ($f_)\n" 1
  	fi
 	
 }
 
 
 ################################################################################
-add_yaml_property() {
-	# <file> <property name> {<value0> <value1> ... }
+set_yaml_property() {
+	# <file> <property> <value>
 
 	process_yaml_file_parameter_ $@
  	
-	if [ $# -lt 2 ]; then
- 		printf "Should pass propertyname and value\n" >&2
- 		exit 1
+	if [ $# -ne 3 ]; then
+ 		error_and_exit_ "Should pass file, propertyname and value\n" 1
+	fi
+	
+	f_="$1"
+	p_="$2"
+	v_="$3"
+ 	
+ 	(has_yaml_property $@ 2> /dev/null)
+	if [ "$?" -eq '0' ]; then
+		delete_yaml_property "$f_" "$p_"
 	fi
  	
+ 	echo "$p_: $v_" >> "$f_"
+}
+
+
+################################################################################
+set_yaml_list_property() {
+	# <file> <property> {<value0> <value1>...}
+
+	process_yaml_file_parameter_ $@
+ 	
+	if [ $# -lt 3 ]; then
+ 		error_and_exit_ "Should pass file, propertyname and values\n" 1
+	fi	
+ 	
+ 	f_="$1"
+ 	shift
+ 	p_="$1"
+ 	shift
+ 	
+ 	(has_yaml_property "$f_" "$p_" 2> /dev/null)
+	if [ "$?" -eq '0' ]; then
+		delete_yaml_property "$f_" "$p_"
+	fi
+ 	
+ 	# from now on it's all values
+	v_='' 	
+ 	while [ $# -ne 0 ]; do
+  		v_+=" - $1\n"
+  		shift
+  	done
+  	# make sure we interpret \n's
+  	# FIXME: this can have side effects with escaped chars in properties
+	printf "$p_:\n$v_" >> "$f_"
+	
 }
 
 
 ################################################################################
 has_yaml_property() {
+	# <file> <property name>
 
 	process_yaml_file_parameter_ $@
 
-	if [ $# -ne 2 ]; then
- 		printf "Should pass propertyname only\n" >&2
- 		exit 1
+	if [ "$#" -lt '2' ]; then
+ 		error_and_exit_ "Should pass propertyname\n" 1
 	fi
 
-	p_="$2"
+	p_="$2:"
 	grep -q "$p_" "$f_"
-	if [ "$?" -ne 0 ]; then
-		printf "Property not found ($?)\n" >&2
-		exit 1
+	if [ "$?" -ne '0' ]; then
+		error_and_exit_ "Property '$p_' not found ($?)\n" 1
 	fi
 	
 	exit 0
@@ -93,48 +133,185 @@ has_yaml_property() {
 
 
 ################################################################################
-get_yaml_property() {
-	$(has_yaml_property $@)
+is_singleline_() {
+	# <file> <property name>
+	
+	f_="$1"
+	property_oneline_expr_="$2:\ +"
+
+	grep -q -E "$property_oneline_expr_" "$f_"
+	if [ "$?" -eq '0' ]; then
+		exit 0
+	else
+		exit 1
+	fi
+}
+
+
+################################################################################
+get_first_line_of_list_() {
+	# <file> <list property name>
+
+	f_="$1"
+	property_="$2:"
+	
+	i_=$(grep -E -n "$property_" "$f_" | awk -F: '{print $1}')
+	i_=$(($i_ + 1))
+	echo -n "$i_"
 
 }
 
 
 ################################################################################
-add_zookeeper_addresses() {
- 	# $1 file {address address ...}
-	a=a
-  
+get_yaml_property() {
+	# <file> <property>
+	
+	(has_yaml_property $@)
+	e_="$?"
+	if [ "$e_" -ne '0' ]; then
+		exit $e_
+	fi
+	
+	f_="$1"
+	p_="$2"
+
+	$(is_singleline_ $@)
+	if [ "$?" -eq '0' ]; then
+
+		# single line, just echo the property
+		property_="$p_:\ +"
+		value_=$(grep -E "$property_" "$f_" | sed -r "s/$property_//")		
+		echo -n "$value_"
+
+	else
+		# multiline, we tackle lists for now
+
+		i_=$(get_first_line_of_list_ $@)
+		is_list_=true
+		value_=''
+		list_expr_='^\ +\-\ +'
+		
+		while $is_list_; do
+		
+			# check if current line is still a list value and add to output accordingly
+			line_=$(awk "FNR==$i_" "$f_")
+			echo  "$line_" | grep -E -q "$list_expr_"
+			if [ "$?" -eq '0' ]; then
+				v_=$(echo "$line_" | sed -r "s/$list_expr_//")
+				value_+=" $v_"
+				i_=$(( $i_ + 1 ))		
+			else
+				is_list_=false
+			fi
+		done
+
+		# we cut the first char as it's a space that has been added by the loop
+		echo "$value_" |cut -c2-
+
+	fi
+
+}
+
+
+################################################################################
+delete_yaml_property() {
+
+	(has_yaml_property $@)
+	e_="$?"
+	if [ "$e_" -ne '0' ]; then
+		exit $e_
+	fi
+
+	f_="$1"
+	p_="$2"
+
+	$(is_singleline_ $@)
+	if [ "$?" -eq '0' ]; then
+
+		# single line, just delete the line
+		property_="$p_:\ +"
+		sed -r -i "/$property_/d" "$f_"
+
+	else
+
+		i_=$(get_first_line_of_list_ $@)
+		is_list_=true
+		list_expr_='^\ +\-\ +'
+		
+		while $is_list_; do
+		
+			# check if current line is still a list value and delete
+			line_=$(awk "FNR==$i_" "$f_")
+			echo  "$line_" | grep -E -q "$list_expr_"
+			if [ "$?" -eq '0' ]; then
+				sed -i "${i_}d" "$f_" 
+				# notice we don't increment as number of lines has decreased :)
+			else
+				is_list_=false
+			fi
+		done
+		
+		# delete list variable name
+		property_="$p_:$"
+		sed -r -i "/$property_/d" "$f_"
+		
+	fi
+
+}
+
+
+################################################################################
+set_zookeeper_addresses() {
+ 	# <file> {address address ...}
+ 	f_="$1"
+ 	shift
+	set_yaml_list_property "$f_" 'storm.zookeeper.servers' $@
+}
+
+
+################################################################################
+set_nimbus_host() {
+ 	f_="$1"
+ 	shift
+	set_yaml_property "$f_" 'nimbus.host' "$1"
+}
+
+
+################################################################################
+set_supervisor_ports() {
+ 	# <file> {address address ...}
+ 	f_="$1"
+ 	shift
+	set_yaml_list_property "$f_" 'supervisor.slots.ports' $@
 }
 
 
 zookeeper_=''
 nimbus_=''
+supervisors_=''
+storm_yaml_='${storm.configfolder_}/storm.yaml'
 
-while getopts "hzn" flag
+while getopts "hzn:s" flag
 do
   case $flag in
 	h) print_usage_exit ;;
 	z) zookeeper_='yes';;
 	n) nimbus_='yes';;
+	s) supervisor_='yes';;
 	\?) echo "Invalid option: -$OPTARG (use -h for help)" >&2;;
   esac
 done
 shift $((OPTIND-1))
 
 if [[ "$zookeeper_" == 'yes' ]]; then
-	# configure storm zookeeper servers
-	a=a
-#if [ $#]
+	set_zookeeper_addresses "$storm_yaml_" $@
 
 elif [[ "$nimbus_" == 'yes' ]]; then
-	# specify nimbus address
-	a=a
+		set_nimbus_host "$storm_yaml_" "$1"
 
+elif [[ "$supervisor_" == 'yes' ]]; then
+	set_supervisor_ports "$storm_yaml_" $@
+	
 fi
 
-
-
-
-
-
-
+# fix for stupid vmware bug
--- meteorit-backend-storm/src/test/bash/meteorit-backend-storm-test.sh
+++ meteorit-backend-storm/src/test/bash/meteorit-backend-storm-test.sh
@@ -20,7 +20,14 @@ SHUNIT2_=/usr/share/shunit2/shunit2
 oneTimeSetUp() {
 	workfolder_="$SHUNIT_TMPDIR"
 	yaml_="$workfolder_/test.yaml"
-	source '${project.build.outputDirectory}/meteorit-backend-storm-setup.sh'
+
+	if [ "$#" -eq '1' ]; then
+		# used for development
+		source "$1"
+	else
+		source '${project.build.outputDirectory}/meteorit-backend-storm-setup.sh'
+	fi
+
 }
 
 
@@ -30,54 +37,136 @@ setUp() {
 
 }
 
-test_add_yaml_property() {
+test_set_yaml_property() {
 
-	$(add_yaml_property) 
+	$(set_yaml_property 2> /dev/null) 
 	assertEquals 'No file parameter given, should die' '1' "$?"
 
-	$(add_yaml_property 'NONEXISTANTFILE')
+	$(set_yaml_property 'NONEXISTANTFILE' 2> /dev/null)
 	assertEquals 'Nonexistant file, should die' '1' "$?"
 
-	$(add_yaml_property "$yaml_")
+	$(set_yaml_property "$yaml_" 2> /dev/null)
 	assertEquals 'No property specified, should die' '1' "$?"
+
+	$(set_yaml_property "$yaml_" 'newproperty' 2> /dev/null)
+	assertEquals 'Not found property, should die' '1' "$?"
+	
+	$(has_yaml_property "$yaml_" 'newproperty' 2> /dev/null)
+	assertEquals 'Before adding the property, should die' '1' "$?"
+	set_yaml_property "$yaml_" 'newproperty' '54321'
+	$(has_yaml_property "$yaml_" 'newproperty')
+	assertEquals 'Just added the property, should not die' '0' "$?"
+	v_=$(get_yaml_property "$yaml_" 'newproperty')
+	assertEquals 'Set property does not have correct value' '54321' "$v_"
+
+	set_yaml_property "$yaml_" 'newproperty' '654321'
+	v_=$(get_yaml_property "$yaml_" 'newproperty')
+	assertEquals 'Resetted property does not have correct value' '654321' "$v_"
+	
+}
+
+
+test_set_yaml_list_property() {
+
+	$(set_yaml_list_property 2> /dev/null) 
+	assertEquals 'No file parameter given, should die' '1' "$?"
+
+	$(set_yaml_list_property 'NONEXISTANTFILE' 2> /dev/null)
+	assertEquals 'Nonexistant file, should die' '1' "$?"
+
+	$(set_yaml_list_property "$yaml_" 2> /dev/null)
+	assertEquals 'No property specified, should die' '1' "$?"
+
+	$(has_yaml_property "$yaml_" 'newproperty' 2> /dev/null)
+	assertEquals 'Before adding the property, should die' '1' "$?"
+	set_yaml_list_property "$yaml_" 'newproperty' '1' '2' '3'
+	v_=$(get_yaml_property "$yaml_" 'newproperty')
+	assertEquals 'List property is not recovered properly' '1 2 3' "$v_"
+
+	set_yaml_list_property "$yaml_" 'newproperty' '1' '2' '3' '4'
+	v_=$(get_yaml_property "$yaml_" 'newproperty')
+	assertEquals 'List property is not resetted properly' '1 2 3 4' "$v_"
 
 }
 
 
 test_has_yaml_property() {
 
-	$(has_yaml_property >&2 /dev/null) 
+	$(has_yaml_property 2> /dev/null) 
 	assertEquals 'No file parameter given, should die' '1' "$?"
 
-	$(has_yaml_property 'NONEXISTANTFILE' >&2 /dev/null)
+	$(has_yaml_property 'NONEXISTANTFILE' 2> /dev/null)
 	assertEquals 'Nonexistant file, should die' '1' "$?"
 
-	$(has_yaml_property "$yaml_" >&2 /dev/null)
+	$(has_yaml_property "$yaml_" 2> /dev/null)
 	assertEquals 'No property specified, should die' '1' "$?"
 
-	$(has_yaml_property "$yaml_" 'notfoundproperty' >&2 /dev/null)
+	$(has_yaml_property "$yaml_" 'notfoundproperty' 2> /dev/null)
 	assertEquals 'Not found property, should die' '1' "$?"
 	
-	$(has_yaml_property "$yaml_" 'atribute.text')
-	assertEquals 'Existant property, should not die' '0' "$?"	
-	
+	$(has_yaml_property "$yaml_" 'value.text')
+	assertEquals 'Existing property, should not die' '0' "$?"	
+
 }
+
 
 test_get_yaml_property() {
 
-	$(get_yaml_property) 
-	assertEquals 'No parameter given should die' '1' "$?"
+	$(get_yaml_property 2> /dev/null) 
+	assertEquals 'No parameter given, should die' '1' "$?"
 
-	$(get_yaml_property 'NONEXISTANTFILE')
-	assertEquals 'Nonexistant file should die' '1' "$?"
+	$(get_yaml_property 'NONEXISTANTFILE' 2> /dev/null)
+	assertEquals 'Nonexistant file, should die' '1' "$?"
 
-	$(get_yaml_property "$yaml_")
-	assertEquals 'No property specified should die' '1' "$?"
+	$(get_yaml_property "$yaml_" 2> /dev/null)
+	assertEquals 'No property specified, should die' '1' "$?"
 
-	$(get_yaml_property "$yaml_" 'notfoundproperty')
-	assertEquals 'No property should die' '1' "$?"
+	$(get_yaml_property "$yaml_" 'notfoundproperty' 2> /dev/null)
+	assertEquals 'Property does not exist, should die' '1' "$?"
+
+	v_=$(get_yaml_property "$yaml_" 'value.text')
+	assertEquals 'Text property is not recovered properly' '"foo"' "$v_"
+
+	v_=$(get_yaml_property "$yaml_" 'value.int')
+	assertEquals 'Numeric property is not recovered properly' '12345' "$v_"
+
+	v_=$(get_yaml_property "$yaml_" 'value.list')
+	assertEquals 'List property is not recovered properly' '1 2 3 4' "$v_"
 
 }
+
+
+test_delete_yaml_property() {
+
+	$(delete_yaml_property 2> /dev/null) 
+	assertEquals 'No parameter given, should die' '1' "$?"
+
+	$(delete_yaml_property 'NONEXISTANTFILE' 2> /dev/null)
+	assertEquals 'Nonexistant file, should die' '1' "$?"
+
+	$(delete_yaml_property "$yaml_" 2> /dev/null)
+	assertEquals 'No property specified, should die' '1' "$?"
+
+	$(delete_yaml_property "$yaml_" 'notfoundproperty' 2> /dev/null)
+	assertEquals 'Property does not exist, should die' '1' "$?"
+
+	$(has_yaml_property "$yaml_" 'value.text')
+	assertEquals 'Existing property before deletion, should not die' '0' "$?"
+	delete_yaml_property "$yaml_" 'value.text'
+	$(has_yaml_property "$yaml_" 'value.text'  2> /dev/null)
+	assertEquals 'Deleted property, should die' '1' "$?"	
+	v_=$(get_yaml_property "$yaml_" 'value.int')
+	assertEquals 'It seems delete deleted more than expected' '12345' "$v_"
+
+	delete_yaml_property "$yaml_" 'value.list'
+	$(has_yaml_property "$yaml_" 'value.list'  2> /dev/null)
+	assertEquals 'Deleted list property, should die' '1' "$?"
+	v_=$(get_yaml_property "$yaml_" 'value.int')
+	assertEquals 'It seems list delete deleted more than expected' '12345' "$v_"
+		
+}
+
+
 
 . $SHUNIT2_
 
